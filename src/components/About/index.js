54a34017a5f98e8cf919f43940be323799253f38
Vsoto7697 <109629515+Vsoto7697@users.noreply.github.com>
2023-02-08 17:15:34 -0500

    updated bio

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Selfie from '../../assets/images/vicandmaxselfie.jpeg';
 
 const About = () => {
   return (
     <section className="about">
       <h1 id="about">About Me</h1>
       <p>
-       
+      Hello, my name is Victor Soto. I recently graduated from Columbia University's Full Stack Web Development bootcamp program and received my certificate of completion. I am currently residing in New York, NY. I am actively looking for work as a Junior Web Developer. Watching Baseball, collecting sneakers, playing video games, and hiking with my dog are some of my hobbies when I'm not creating websites.
       </p>
+      <div className="aboutmephoto">
+        <img src={Selfie} alt="self portrait" />
+      </div>
       <p>
         
       </p>
